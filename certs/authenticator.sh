5db7cd5ab345807c2f220e3022b80ebb004947b3
wyworx <rcd@wyworx.com>
2021-11-11 15:05:13 -0500

    Customize manifests, flux and setup GitHub CD workflow

--- certs/authenticator.sh
+++ certs/authenticator.sh
@@ -4,8 +4,14 @@ STORAGE_ACCOUNT_NAME=$1
 
 echo $CERTBOT_VALIDATION>${CERTBOT_TOKEN}.txt
 
+echo ${CERTBOT_TOKEN}
+
+cat ${CERTBOT_TOKEN}.txt
+
 echo "Uploading validation token to $STORAGE_ACCOUNT_NAME"
 
 az storage blob upload --account-name $STORAGE_ACCOUNT_NAME -c \$web -n "${CERTBOT_TOKEN}" -f "./${CERTBOT_TOKEN}.txt" --auth-mode key --only-show-errors
 
-rm ${CERTBOT_TOKEN}.txt
+echo ${CERTBOT_TOKEN}
+
+#rm ${CERTBOT_TOKEN}.txt
