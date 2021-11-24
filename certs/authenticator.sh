0aa054168a3ce6452b9d7432e69eb73f910b84bb
wyworx <rcd@wyworx.com>
2021-11-24 15:21:28 -0500

    Customize manifests, flux and setup GitHub CD workflow

--- certs/authenticator.sh
+++ certs/authenticator.sh
@@ -4,14 +4,16 @@ STORAGE_ACCOUNT_NAME=$1
 
 echo $CERTBOT_VALIDATION>${CERTBOT_TOKEN}.txt
 
-echo ${CERTBOT_TOKEN}
-
-cat ${CERTBOT_TOKEN}.txt
 
 echo "Uploading validation token to $STORAGE_ACCOUNT_NAME"
 
-az storage blob upload --account-name $STORAGE_ACCOUNT_NAME -c \$web -n "${CERTBOT_TOKEN}" -f "./${CERTBOT_TOKEN}.txt" --auth-mode key --only-show-errors
+#az storage blob upload --account-name $STORAGE_ACCOUNT_NAME -c \$web -n "${CERTBOT_TOKEN}" -f "./${CERTBOT_TOKEN}.txt" --auth-mode key --only-show-errors
 
-echo ${CERTBOT_TOKEN}
+./azcopy copy "./${CERTBOT_TOKEN}.txt" "https://regionhq6kwyzw2one4.blob.core.windows.net/\$web/${CERTBOT_TOKEN}?sv=2020-08-04&ss=bfqt&srt=sco&sp=rwdlacupitfx&se=2021-12-03T22:14:31Z&st=2021-11-22T14:14:31Z&sip=100.34.230.121&spr=https&sig=DoTb54mpM1wGeo9Zkg%2FAdD0hxlgS38VHBtOIz0vR1gU%3D"
+#./azcopy copy "./${CERTBOT_TOKEN}.txt" "https://regionhq6kwyzw2one4.blob.core.windows.net/\$web/.well-known/acme-challenge/${CERTBOT_TOKEN}?sv=2020-08-04&ss=bfqt&srt=sco&sp=rwdlacupitfx&se=2021-12-03T22:14:31Z&st=2021-11-22T14:14:31Z&sip=100.34.230.121&spr=https&sig=DoTb54mpM1wGeo9Zkg%2FAdD0hxlgS38VHBtOIz0vR1gU%3D"
+
+#./azcopy copy $source $blob
+
+sleep 10
 
 #rm ${CERTBOT_TOKEN}.txt
--- certs/letsencrypt-pip-cert-generation.sh
+++ certs/letsencrypt-pip-cert-generation.sh
@@ -17,20 +17,23 @@ echo "Creating temporary Resource Group $RGNAME"
 az group create -n $RGNAME -l $LOCATION
 
 echo "Deploying Azure resources used in validation; this may take 20 minutes."
-az deployment group create -g $RGNAME -f ./certs/lets-encrypt-resources-custom.json -p location=${LOCATION} subdomainName=${SUBDOMAIN} ipResourceId=${IP_RESOURCE_ID}
+az deployment group create -g $RGNAME -u "https://raw.githubusercontent.com/mspnp/letsencrypt-pip-cert-generation/a4f89d43004d4250af02c2bc2194d62467690422/resources-stamp.json" -p location=${LOCATION} subdomainName=${SUBDOMAIN} ipResourceId=${IP_RESOURCE_ID}
 
-STORAGE_ACCOUNT_NAME=$(az deployment group show -g $RGNAME -n $SUBDOMAIN --query properties.outputs.storageAccountName.value -o tsv)
+STORAGE_ACCOUNT_NAME=$(az deployment group show -g $RGNAME -n resources-stamp --query properties.outputs.storageAccountName.value -o tsv)
 
 echo "Enabling web hosting on $STORAGE_ACCOUNT_NAME"
 az storage blob service-properties update --account-name $STORAGE_ACCOUNT_NAME --static-website true --auth-mode login
 
 echo "Uploading placeholder to storage"
 echo pong>ping.txt
-az storage blob upload --account-name $STORAGE_ACCOUNT_NAME -c \$web -n ping -f ./ping.txt --auth-mode key
+#az storage blob upload --account-name $STORAGE_ACCOUNT_NAME -c \$web -n ping -f ./ping.txt --auth-mode key
+
+azcopy copy "./ping.txt" "https://regionwve3oxteiqbru.blob.core.windows.net/\$web/ping?sv=2020-08-04&ss=bfqt&srt=sco&sp=rwdlacupitfx&se=2021-12-05T00:46:41Z&st=2021-11-24T16:46:41Z&spr=https,http&sig=aLCNDn0qgeOosPZ7fzz3lQo5%2BFFHtgTjNpXti44o1Mg%3D"
 
 echo "Starting cert generation and validation"
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
-certbot certonly --manual --manual-auth-hook "${DIR}/authenticator.sh ${STORAGE_ACCOUNT_NAME}" -d $FQDN --config-dir ./certs/output/etc/letsencrypt --work-dir ./certs/output/var/lib/letsencrypt --logs-dir ./certs/output/var/log/letsencrypt
+#certbot certonly --manual --manual-auth-hook "${DIR}/authenticator.sh ${STORAGE_ACCOUNT_NAME}" -d $FQDN --config-dir ./certs/output/etc/letsencrypt --work-dir ./certs/output/var/lib/letsencrypt --logs-dir ./certs/output/var/log/letsencrypt
+certbot certonly --manual -d $FQDN --config-dir ./certs/output/etc/letsencrypt --work-dir ./certs/output/var/lib/letsencrypt --logs-dir ./certs/output/var/log/letsencrypt
 
 openssl pkcs12 -export -out ${SUBDOMAIN}.pfx -inkey ./certs/output/etc/letsencrypt/live/${FQDN}/privkey.pem -in ./certs/output/etc/letsencrypt/live/${FQDN}/cert.pem -certfile ./certs/output/etc/letsencrypt/live/${FQDN}/chain.pem -passout pass:
 
